0049a41dd127c9a4bfe3ba66f13470087aaa2e91
emalgithub <emalgithub@gmail.com>
2024-05-14 19:38:36 -0400

    some resources deleted

--- src/test/java/com/automation/test/VisualTesting.java
+++ src/test/java/com/automation/test/VisualTesting.java
@@ -7,8 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class VisualTesting {
 	
 	public static void main(String[] args) {
-		
-//		System.setProperty("webdriver.chrome.driver", ".//driver//chromedriver.exe");
+
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.bestbuy.com");
 		driver.manage().window().maximize();
